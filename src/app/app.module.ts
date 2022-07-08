e9a75e25c070aeca26540b0005aaf2cd977ceeb4
riki89 <saikoudiallo59@gmail.com>
2022-07-07 20:24:00 -0500

    home-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavabarComponent } from './navabar/navabar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavabarComponent
   ],
   imports: [
     BrowserModule,
